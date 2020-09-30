0d750f6b03e63340c56b26fbd0e2e9daf6f29e9a
Matthew Vielkind <matthewvielkind@Matthews-MBP.fios-router.home>
2020-09-30 08:25:14 -0400

    Make working demo

--- bot.py
+++ bot.py
@@ -1,21 +1,77 @@
 import trello
+import json
+import os
+from twilio.rest import Client 
 from flask import Flask, request
 
 app = Flask(__name__)
 
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCONUT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_FROM_NUMBER = os.getenv("TWILIO_FROM_NUMBER")
+TWILIO_TO_NUMBER = os.getenv("TWILIO_TO_NUMBER")
+
+
 
 @app.route("/add-to-waitlist", methods=["POST"])
 def add_to_waitlist():
-	print(request.values)
+	memory = json.loads(request.values["Memory"])
+	phone_number = memory["twilio"]["sms"]["From"]
+
+	# Mask phone number for demo.
+	phone_number = "+15555555"
+
+	customer_answers = memory["twilio"]["collected_data"]["customer_info"]["answers"]
+
+	customer_name = customer_answers["customer_name"]["answer"]
+	party_size = customer_answers["party_size"]["answer"]
+
+	# Build the card title that will be the customer name and party size.
+	title = f"{customer_name}: {party_size}"
+
+	# Build the card description from the customer answers.
+	description = "\n".join([f"{q.replace('_', ' ').title()}: {a['answer']}" for q, a in customer_answers.items()])
+	description = "\n".join([description, f"Phone Number: {phone_number}"])
 
 	# Trigger creating a card in Trello.
+	card = trello.create_card_in_waitlist(title, description)
+	card_id = json.loads(card.text)["id"]
+	print(card_id)
 
+	# Add a quick attachement.
+	trello.add_attachment_to_card(card_id)
+
+	# Update custom fields for the card.
+	response = trello.update_card_custom_fields(card_id, "name", customer_name)
+	print(response.text)
+	response = trello.update_card_custom_fields(card_id, "phone_number", phone_number)
+	print(response.text)
 
 	return {
-		"actions": {
-			"say": "Added to waitlist."
-		}
+		"actions": [
+			{"say": "Added to waitlist."}
+		]
 	}
+
+
+@app.route("/table-is-ready", methods=["POST"])
+def table_is_ready():
+	dat = json.loads(request.data)
+
+	to_number = dat["phone_number"]
+	# Mask the phone number for now.
+	to_number = TWILIO_TO_NUMBER
+	msg = f"Howdy {dat['name']}! Your table is ready!"
+
+	twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+	response = twilio_client.messages.create(
+		body=msg,
+		from_=TWILIO_FROM_NUMBER,
+		to=to_number
+	)
+
+	return {"STATUS": 200}
 
 
 if __name__ == "__main__":
--- trello.py
+++ trello.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 from dotenv import load_dotenv
 load_dotenv()
@@ -6,10 +7,16 @@ load_dotenv()
 
 TRELLO_API_KEY = os.getenv("TRELLO_API_KEY")
 TRELLO_TOKEN = os.getenv("TRELLO_TOKEN")
-TRELLO_BOARD = "5f7312a009963d58f67798cd"
+TRELLO_BOARD = os.getenv("TRELLO_BOARD_ID")
+TRELLO_WAITLIST_ID = os.getenv("TRELLO_WAITLIST_ID")
+
+TRELLO_FIELDS = {
+	"name": os.getenv("TRELLO_FIELD_NAME"),
+	"phone_number": os.getenv("TRELLO_FIELD_NUMBER")
+}
 
 
-def create_card_in_waitlist():
+def create_card_in_waitlist(title, desc):
 	"""
 	Create a new customer card in the waitlist.
 	"""
@@ -18,8 +25,9 @@ def create_card_in_waitlist():
 	query = {
 		'key': TRELLO_API_KEY,
 		'token': TRELLO_TOKEN,
-		'idList': "test",
-		'name': 'Created with Python'
+		'idList': TRELLO_WAITLIST_ID,
+		'name': title,
+		'desc': desc
 	}
 
 	response = requests.request(
@@ -28,12 +36,37 @@ def create_card_in_waitlist():
 		params=query
 	)
 
-	print(response.text)
+	return response
+
+
+def update_card_custom_fields(card_id, field_name, value):
+	custom_field_id = TRELLO_FIELDS[field_name]
+
+	url = f"https://api.trello.com/1/cards/{card_id}/customField/{custom_field_id}/item"
+	
+	query = {
+		'key': TRELLO_API_KEY,
+		'token': TRELLO_TOKEN,
+		'modelType': 'card',
+		'value': {
+			'text': value
+		}
+	}
+
+	response = requests.request(
+   		"PUT",
+   		url,
+   		json=query
+	)
+
+	return response
 
 
 def get_lists_on_board():
 	"""
 	Get all the of the lists on a board.
+
+	returns: a list of all the lists that are included on the board.
 	"""
 	url = f"https://api.trello.com/1/boards/{TRELLO_BOARD}/lists"
 
@@ -48,4 +81,59 @@ def get_lists_on_board():
 		params=query
 	)
 
-	print(response.text)
+	return json.loads(response.text)
+
+
+def add_attachment_to_card(card_id):
+	url = f"https://api.trello.com/1/cards/{card_id}/attachments"
+
+	headers = {
+ 	  "Accept": "application/json"
+	}
+
+	query = {
+		'key': TRELLO_API_KEY,
+		'token': TRELLO_TOKEN,
+		'url': "https://www.theyoungfolks.com/wp-content/uploads/2016/06/Marlin-and-Nemo.jpg"
+	}
+
+	response = requests.request(
+		"POST",
+		url,
+		params=query
+	)
+
+
+def create_custom_fields():
+	"""
+	Add custom fields to the board for customer name and phone number.
+
+	Having these custom fields will allow Trello to use these fields in the messages that are sent.
+	"""
+	url = "https://api.trello.com/1/customFields"
+
+	headers = {
+		"Accept": "application/json"
+	}
+
+	base_query = {
+		'key': TRELLO_API_KEY,
+		'token': TRELLO_TOKEN,
+		'idModel': TRELLO_BOARD,
+		'modelType': 'board',
+		'type': 'text',
+		'pos': 1,
+		'display_cardFront': "false"
+	}
+
+	for f in ["name", "phone_number"]:
+		base_query["name"] = f
+
+		response = requests.request(
+			"POST",
+			url,
+			headers=headers,
+			params=base_query
+		)
+
+		print(json.loads(response.text))
